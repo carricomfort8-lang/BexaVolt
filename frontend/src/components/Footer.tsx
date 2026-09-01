cfca4a16014db04b8b78fdf6bd962136e41b0335
Raheel <raheel2k29@github.com>
2026-09-01 05:58:29 +0500

    Update Get a Quote buttons in Hero and Footer to link to /quote

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -39,7 +39,7 @@ const Footer = () => {
               </div>
               
               <div className={styles.btnAndTrust}>
-                <Link href="#quote" className="btn-primary">
+                <Link href="/quote" className="btn-primary">
                   GET A FREE QUOTE
                   <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9 18L15 12L9 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
